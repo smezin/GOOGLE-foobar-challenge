7949ba206f19c858797b91792492016a1ec3a55b
shahafme <shahafme@matrix.co.il>
2022-02-15 23:02:51 +0200

    improved

--- foobarC4/sandbox.py
+++ foobarC4/sandbox.py
@@ -1,43 +1,45 @@
-from distutils.log import ERROR
-from itertools import permutations
 
 def solution (times, time_limit):
     """
-    1. Get the number of bunnies
-    2. Set a list of bunnies id's
-    3. Invoke bellman-ford of times adjacency matrix
-    4. In case we detect negative cycle HOORAY! we can make time to save all the bunnies 
-    5. Otherwise, we check for possible path, starting with longest possible paths (cause we want to save them all) 
-    6. Once path is found (which because of step 5 is equally good, or better, than any other path) it is returned 
-    7. If none found we return an empty list (and best of luck wishes for the running bunnies)
+    1.  Get the number of bunnies
+    2.  Set a list of bunnies positions
+    3.  create time durations matrix
+    4.  In case we detect negative cycle HOORAY! we can make time to save all the bunnies. BUT we only check for possible negative 
+            paths starting in our start row, since we can't just spawn anywhere!
+    5.  Otherwise, we check for possible picking order, starting with longest possible paths (cause we want to save them all)
+            always beginning at start and finishing at bulkhead
+    6.  Once path is found (which because of step 5 is equally good, or better, than any other path) it is returned 
+    7.  If none found we return an empty list (and best of luck wishes for the running bunnies)
     """
-    # n of bunnies equals n of times minus start and bulkhead
     n_bunnies = len(times) - 2
-    bunnies = [bunny for bunny in range(1, n_bunnies+1)]
-    distances = bellman_ford(times)
-    if update_distance(distances, distances[0], break_on_update=True) == NEG_PATH_DETECTED:
-        print('neg')
+    bunnies_position = [bunny for bunny in range(1, n_bunnies+1)]
+    time_durations_matrix = create_time_durations_matrix(times)
+
+    if update_paths(time_durations_matrix, time_durations_matrix[0], break_on_update=True) == NEG_PATH_DETECTED:
         return list(range(n_bunnies))
-    for i in range (n_bunnies, 0, -1):
-        for perm in all_perms(bunnies, i):
-            # i goes from longest paths to shortest
-            time = get_path_time(perm, distances)
-            if time <= time_limit:
-                return [index - 1 for index in sorted(perm)]
+
+    for n_bunnies_to_pick in range (n_bunnies, 0, -1):
+        for picking_order in all_perms(bunnies_position, n_bunnies_to_pick):                    # n_bunnies_to_pick goes from most bunnies to least
+            path_duration = sum_path_durations(picking_order, time_durations_matrix)            # if path, by given picking order, satifsy time_limit restriction, return it 
+            if path_duration <= time_limit:
+                return [index - 1 for index in sorted(picking_order)]                          
     return[]
     
-def get_path_time(bunnies, graph):
-    time = 0
-    time += graph[0][bunnies[0]] #starting time
-    time += graph[bunnies[-1]][len(graph)-1] #transition to bulkhead time
-    for i in range(1, len(bunnies)):
-        #path time
-        u = bunnies[i-1]
-        v = bunnies[i]
-        time += graph[u][v]
-    return time
+def sum_path_durations(picking_order, time_durations_matrix):
+    """
+    For given picking order (bunnies permutation) sum the times from start to first bunnies,
+    then each edge to the following bunny, finally from last bunny to bulkhead
+    """
+    path_duration = 0
+    path_duration += time_durations_matrix[0][picking_order[0]]                                 #starting time to first bunny
+    for i in range(1, len(picking_order)):                                                      #picking up bunnies path time
+        u = picking_order[i-1] 
+        v = picking_order[i]
+        path_duration += time_durations_matrix[u][v]
+    path_duration += time_durations_matrix[picking_order[-1]][len(time_durations_matrix)-1]     #getting last bunny to bulkhead time
+    return path_duration
 
-def bellman_ford(graph):
+def create_time_durations_matrix(graph):
     """
     the algorithm initializes the distance to the source to 0 and all other nodes to infinity. 
     Then for all edges, if the distance to the destination can be shortened by taking the edge, 
@@ -46,31 +48,21 @@ def bellman_ford(graph):
     Since the longest possible path without a cycle can as many edges as the graph size as represented as adjacency matrix, 
     the edges must be scanned graph len times to ensure the shortest path has been found for all nodes
     """
-    distances = []
-    for vertex in range(len(graph)):
-        distances.append(find_distance(graph, vertex))
-    return distances
+    durations_matrix = []
+    for start_point in range(len(graph)):
+        durations_matrix.append(find_distance(graph, start_point))
+    return durations_matrix
 
-def find_distance(graph, source):
+def find_distance(graph, start_point):
     n = len(graph)
     distance = [float('inf')] * n       #Initialize the distance to all vertices to infinity
-    distance[source] = 0                #The distance from the source to itself is, of course, zero
+    distance[start_point] = 0           #The distance from the start_point to itself is zero
     #for i in range(n):
-    distance = update_distance(graph, distance)
-    
+    distance = update_paths(graph, distance)
     return distance
 
-# def negative_cycles(graph):
-#     distance = graph[0] #starting row
-#     n = len(graph)
-#     for u in range(n):
-#         for v in range(n):
-#             weight = graph[u][v]
-#             if distance[u] + weight < distance[v]:
-#                 return True
-#     return False
 
-def update_distance(graph, distance, break_on_update=False):
+def update_paths(graph, distance, break_on_update=False):
     n = len(graph)
     for u in range(n):
         for v in range(n):
@@ -82,6 +74,9 @@ def update_distance(graph, distance, break_on_update=False):
     return distance
 
 def all_perms(iterable, r=None):
+    """
+    As implemented by 'itertools'
+    """
     pool = tuple(iterable)
     n = len(pool)
     r = n if r is None else r
