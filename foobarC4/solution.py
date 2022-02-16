517b9f5b67707ee564dd84273ae6be7519261ae4
shahafme <shahafme@matrix.co.il>
2022-02-16 10:40:14 +0200

    submitted c4 solution

--- foobarC4/solution.py
+++ foobarC4/solution.py
@@ -1,71 +1,90 @@
-from itertools import permutations
-
-def solution (times, time_limit):
+def solution(times, times_limit):
     """
-    1. Get the number of bunnies
-    2. Set a list of bunnies id's
-    3. Invoke bellman-ford of times adjacency matrix
-    4. In case we detect negative cycle HOORAY! we can make time to save all the bunnies 
-    5. Otherwise, we check for possible path, starting with longest possible paths (cause we want to save them all) 
-    6. Once path is found (which because of step 5 is equally good, or better, than any other path) it is returned 
-    7. If none found we return an empty list (and best of luck wishes for the running bunnies)
+    Based on Bellman-Ford with few 'tweeks', modifying it's ability to find shortest path for finding 
+    longest path (most bunnies picked) that satisfies times_limit restriction, simply put, we are looking 
+    for a path that is long but quick!
+    1.  Get the number of bunnies
+    2.  Set a list of bunnies positions
+    3.  create time durations matrix
+    4.  In case we detect negative cycle HOORAY! we can make time to save all the bunnies. 
+    5.  Otherwise, we check for possible picking order, starting with longest possible paths (cause we want to save them all)
+            always beginning at start and finishing at bulkhead
+    6.  Once a satisfying path is found (which because of step 5 is best possible path) it is returned 
+    7.  If none found we return an empty list (and best of luck wishes for the running bunnies)
     """
-    # n of bunnies equals n of times minus start and bulkhead
     n_bunnies = len(times) - 2
-    bunnies = [bunny for bunny in range(1, n_bunnies+1)]
-    distances = bellman_ford(times)
-    if negative_cycles(distances):
-        return range(n_bunnies)
-    for i in range (n_bunnies, 0, -1):
-        for perm in all_perms(bunnies, i):
-            # i goes from longest paths to shortest
-            time = get_path_time(perm, distances)
-            if time <= time_limit:
-                return [index - 1 for index in sorted(perm)]
-    return[]
+    bunnies_position = [bunny for bunny in range(1, n_bunnies+1)]
+    times_matrix = create_times_matrix(times)
+    #trying to improve paths after all path times checked, if succeeded there is a negative path available
+    if update_times(times_matrix, times_matrix[0], break_on_update=True) == NEG_PATH_DETECTED:
+        return list(range(n_bunnies))
+
+    for n_bunnies_to_pick in range (n_bunnies, 0, -1):
+        for picking_order in permutations(bunnies_position, n_bunnies_to_pick):     # n_bunnies_to_pick goes from most bunnies to least
+            path_time = sum_path_times(picking_order, times_matrix)         # if path, by given picking order, satifsy time_limit restriction, return it 
+            if path_time <= times_limit:
+                return [index - 1 for index in sorted(picking_order)]                          
+    return []
     
-def get_path_time(bunnies, graph):
-    time = 0
-    time += graph[0][bunnies[0]] #starting time
-    time += graph[bunnies[-1]][len(graph)-1] #transition to bulkhead time
-    for i in range(1, len(bunnies)):
-        #path time
-        u = bunnies[i-1]
-        v = bunnies[i]
-        time += graph[u][v]
-    return time
+def create_times_matrix(adjacency_matrix):
+    """
+    relaxing times (edges) repeatedly for adjacency_matrix * number or rows
+    """
+    times_matrix = []
+    for start_point in range(len(adjacency_matrix)):                         # num of rows, NOT nodes
+        times_matrix.append(relax_times(adjacency_matrix, start_point))
+    return times_matrix
 
-def bellman_ford(graph):
-    distances = []
-    for vertex in range(len(graph)):
-        distances.append(find_distance(graph, vertex))
-    return distances
+def relax_times(adjacency_matrix, start_point):
+    """
+    initializes the duration to the source to 0 and all other nodes to infinity. 
+    Then for all edges, if the duration to the destination can be shortened by taking the edge, 
+    the duration is updated to the new lower value.
+    """
+    v = len(adjacency_matrix)
+    times = [float('inf')] * v       #Initialize the duration to all vertices to infinity
+    times[start_point] = 0           #The duration from the start_point to itself is zero
+    for i in range(v-1):
+        times = update_times(adjacency_matrix, times)
+    return times
 
-def find_distance(graph, source):
-    n = len(graph)
-    distance = [float('inf')] * n       #Initialize the distance to all vertices to infinity
-    distance[source] = 0                #The distance from the source to itself is, of course, zero
-    #lay back and RELAX
-    for i in range(n):
-        for u in range(n):
-            for v in range(n):
-                weight = graph[u][v]
-                if distance[u] + weight < distance[v]:
-                    distance[v] = distance[u] + weight
-    return distance
-
-def negative_cycles(graph):
-    distance = graph[0] #starting row
-    n = len(graph)
+def update_times(adjacency_matrix, times, break_on_update=False):
+    """
+    Bellman-Ford core algorithm, time to each vertex is always an overestimate of the true distance, 
+    and is replaced by the minimum of its old value and the length of a newly found path.
+    This method also uses to detect negative timed path.
+    """
+    n = len(adjacency_matrix)
     for u in range(n):
         for v in range(n):
-            weight = graph[u][v]
-            if distance[u] + weight < distance[v]:
-                return True
-    return False
+            weight = adjacency_matrix[u][v]
+            if times[u] + weight < times[v]:
+                times[v] = times[u] + weight
+                if (break_on_update):
+                    return NEG_PATH_DETECTED
+    return times
 
+def sum_path_times(picking_order, times_matrix):
+    """
+    For a given picking order (bunnies permutation) sum the times from start to first bunnies,
+    then each edge to the following bunny, finally from last bunny to bulkhead
+    """
+    path_time = 0
+    path_time += times_matrix[0][picking_order[0]]                                 #starting time to first bunny
+    for i in range(1, len(picking_order)):                                                      #picking up bunnies path time
+        u = picking_order[i-1] 
+        v = picking_order[i]
+        path_time += times_matrix[u][v]
+    path_time += times_matrix[picking_order[-1]][len(times_matrix)-1]     #getting last bunny to bulkhead time
+    return path_time
 
-def all_perms(iterable, r=None):
+def permutations(iterable, r=None):
+    """
+    As implemented by 'itertools'.
+    The default implementation yields permutations starting with first (lower) of iterable list,
+    hence satisfy the restriction 'If there are multiple sets of bunnies of the same size, 
+    return the set of bunnies with the lowest worker IDs (as indexes)' by default checks order
+    """
     pool = tuple(iterable)
     n = len(pool)
     r = n if r is None else r
@@ -88,5 +107,8 @@ def all_perms(iterable, r=None):
         else:
             return
 
+NEG_PATH_DETECTED = 'negative path detected'
+
 print(solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1))
 print(solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3))
+print(solution([[0, -11, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3))
--- foobarC4/solution_w_prints.py
+++ foobarC4/solution_w_prints.py
@@ -1,13 +1,9 @@
-
-from itertools import permutations
-
-def solution (times, time_limit):
+def solution(times, times_limit):
     """
     1.  Get the number of bunnies
     2.  Set a list of bunnies positions
     3.  create time durations matrix
-    4.  In case we detect negative cycle HOORAY! we can make time to save all the bunnies. BUT we only check for possible negative 
-            paths starting in our start row, since we can't just spawn anywhere!
+    4.  In case we detect negative cycle HOORAY! we can make time to save all the bunnies. 
     5.  Otherwise, we check for possible picking order, starting with longest possible paths (cause we want to save them all)
             always beginning at start and finishing at bulkhead
     6.  Once path is found (which because of step 5 is equally good, or better, than any other path) it is returned 
@@ -16,31 +12,19 @@ def solution (times, time_limit):
     n_bunnies = len(times) - 2
     bunnies_position = [bunny for bunny in range(1, n_bunnies+1)]
     times_matrix = create_times_matrix(times)
-
+    #trying to improve paths after all path times checked, if succeeded there is a negative path available
     if update_times(times_matrix, times_matrix[0], break_on_update=True) == NEG_PATH_DETECTED:
+        print('neg detected')
         return list(range(n_bunnies))
 
     for n_bunnies_to_pick in range (n_bunnies, 0, -1):
-        for picking_order in permutations(bunnies_position, n_bunnies_to_pick):                    # n_bunnies_to_pick goes from most bunnies to least
-            path_time = sum_path_times(picking_order, times_matrix)            # if path, by given picking order, satifsy time_limit restriction, return it 
-            if path_time <= time_limit:
+        for picking_order in permutations(bunnies_position, n_bunnies_to_pick):     # n_bunnies_to_pick goes from most bunnies to least
+            path_time = sum_path_times(picking_order, times_matrix)         # if path, by given picking order, satifsy time_limit restriction, return it 
+            if path_time <= times_limit:
+                print ('picking_order by position-->',picking_order)
                 return [index - 1 for index in sorted(picking_order)]                          
-    return[]
+    return []
     
-def sum_path_times(picking_order, times_matrix):
-    """
-    For a given picking order (bunnies permutation) sum the times from start to first bunnies,
-    then each edge to the following bunny, finally from last bunny to bulkhead
-    """
-    path_time = 0
-    path_time += times_matrix[0][picking_order[0]]                                 #starting time to first bunny
-    for i in range(1, len(picking_order)):                                                      #picking up bunnies path time
-        u = picking_order[i-1] 
-        v = picking_order[i]
-        path_time += times_matrix[u][v]
-    path_time += times_matrix[picking_order[-1]][len(times_matrix)-1]     #getting last bunny to bulkhead time
-    return path_time
-
 def create_times_matrix(adjacency_matrix):
     """
     relaxing times (edges) repeatedly for adjacency_matrix number or rows
@@ -48,7 +32,22 @@ def create_times_matrix(adjacency_matrix):
     times_matrix = []
     for start_point in range(len(adjacency_matrix)):                                            #num of rows, NOT nodes
         times_matrix.append(relax_times(adjacency_matrix, start_point))
+        print(relax_times(adjacency_matrix, start_point))
     return times_matrix
+
+def sum_path_times(picking_order, times_matrix):
+    """
+    For a given picking order (bunnies permutation) sum the times from start to first bunnies,
+    then each edge to the following bunny, finally from last bunny to bulkhead
+    """
+    path_time = 0
+    path_time += times_matrix[0][picking_order[0]]                       #starting time to first bunny
+    for i in range(1, len(picking_order)):                               #picking up bunnies path time
+        u = picking_order[i-1] 
+        v = picking_order[i]
+        path_time += times_matrix[u][v]
+    path_time += times_matrix[picking_order[-1]][len(times_matrix)-1]     #getting last bunny to bulkhead time
+    return path_time
 
 def relax_times(adjacency_matrix, start_point):
     """
@@ -56,11 +55,11 @@ def relax_times(adjacency_matrix, start_point):
     Then for all edges, if the duration to the destination can be shortened by taking the edge, 
     the duration is updated to the new lower value.
     """
-    n = len(adjacency_matrix)
-    times = [float('inf')] * n       #Initialize the duration to all vertices to infinity
+    v = len(adjacency_matrix)
+    times = [float('inf')] * v       #Initialize the duration to all vertices to infinity
     times[start_point] = 0           #The duration from the start_point to itself is zero
-    #for i in range(n):
-    times = update_times(adjacency_matrix, times)
+    for i in range(v-1):
+        times = update_times(adjacency_matrix, times)
     return times
 
 
@@ -75,9 +74,36 @@ def update_times(adjacency_matrix, times, break_on_update=False):
                     return NEG_PATH_DETECTED
     return times
 
+def permutations(iterable, r=None):
+    """
+    As implemented by 'itertools'.
+    The default implementation yields permutations starting with first (lower) of iterable list,
+    hence satisfy the restriction 'If there are multiple sets of bunnies of the same size, 
+    return the set of bunnies with the lowest worker IDs (as indexes)' by default checks order
+    """
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    if r > n:
+        return
+    indices = list(range(n))
+    cycles = list(range(n, n-r, -1))
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i+1:] + indices[i:i+1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
 
 NEG_PATH_DETECTED = 'negative path detected'
-
 
 print(solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1))
 print(solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3))
